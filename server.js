060bd44d08aa77882d3e0702668a3406834cfa44
Ryozen7 <phil.rock1996@gmail.com>
2021-03-10 14:03:07 +0800

    Change some

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get ('/', (req, res)=>{
-	res.send(database.users);
+	res.send('It is working');
 })
 
 app.post('/signin', (req, res) => {handleSignin(req, res, db, bcrypt)})
@@ -32,6 +32,6 @@ app.get('/profile/:id',(req, res) => {handleProfile(req, res, db )})
 app.put('/image', (req, res) => {handleImage(req, res, db )})
 app.post('/imageurl', (req, res) => {handleApiCall(req, res)})
 
-app.listen(3005, () => {
-	console.log('App is running');
+app.listen(process.env.PORT || 3005, () => {
+	console.log('App is running on port ' + process.env.PORT );
 }) 
